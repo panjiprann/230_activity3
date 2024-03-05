105fd79b4e0a6848e9aa5bc99006872b5c1e2312
panjiprann <161429966+panjiprann@users.noreply.github.com>
2024-03-05 18:29:41 +0700

    Add files via upload

---
+++ smithhhh.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+    int bilangan;
+    srand(time(0));
+    bilangan=rand() % 10;
+
+    cout << "nilai awal : " << bilangan << endl;
+
+    if (bilangan >= 5)
+    {
+     bilangan = 2 * bilangan; 
+    }
+
+    cout << "Nilai sekarang = " << bilangan << endl;
+}
